4d2275fa2d280846d8cd4dca356569f32de2ee48
shusaku009 <shusaku009@gmail.com>
2024-02-12 12:09:14 +0900

    Add: タイムトラベル

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ function Square({ value, onSquareClick }) {
 }
 
 const Board = ({ xIsNext, squares, onPlay }) => {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
 
   const handleClick = (i) => {
     if (squares[i] || calculateWinner(squares)) {
@@ -19,8 +17,6 @@ const Board = ({ xIsNext, squares, onPlay }) => {
       nextSquares[i] = "0";
     }
     onPlay(nextSquares);
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
   }
 
   const winner = calculateWinner(squares);
@@ -76,12 +72,34 @@ const calculateWinner = (squares) => {
 const Game = () => {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
 
   const handlePlay = (nextSquares) => {
-    setHistory([...history, nextSquares]);
+    const nextHistory = setHistory([...history, nextSquares]);
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1)
     setXIsNext(!xIsNext);
   }
+
+  const jumpTo = (nextMove) => {
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    )
+  })
 
   return (
     <div className="game">
@@ -89,7 +107,7 @@ const Game = () => {
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-        <ol>{/*TODO*/}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   )
